aca11cf04e63ef892fa306a9c59d53a4457e76e3
JustaDoc <103592528+JustaDoc@users.noreply.github.com>
2025-03-22 21:41:49 -0400

    Update client.py
    removed client_socket.send(chunk) with if else loop

--- client.py
+++ client.py
@@ -21,10 +21,10 @@ def send_file(filename: str, address: (str, int)):
     ret_value = 0
     # get the file size in bytes
     # TODO: section 2 step 2
-    file_size = get_file_size(file_name)
+    file_size = get_file_size(filename)
     # convert file_size to an 8-byte byte string using big endian
     # TODO: section 2 step 3
-    file_size_bytes = file_size.to_bytes(8, 'big')
+    file_size_bytes = file_size.to_bytes(8, byteorder='big')
     # create a TCP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -34,20 +34,23 @@ def send_file(filename: str, address: (str, int)):
         # send the file size in the first 8-bytes followed by the bytes
         # for the file name to server at (IP, PORT)
         # TODO: section 2 step 6
-        client_socket.send(file_size_bytes + file_name.encode)
+        client_socket.send(file_size_bytes + filename.encode())
         # TODO: section 2 step 7
         server_response = client_socket.recv(BUFFER_SIZE)
-        if server_response.decode() != b'go ahead':
+        if server_response.decode() != 'go ahead':
             raise OSError('Bad server response - was not go ahead!')
         # open the file to be transferred
-        with open(file_name, 'rb') as file:
+        with open(filename, 'rb') as file:
             # read the file in chunks and send each chunk to the server
             is_done = False
             while not is_done:
                 # TODO: section 2 step 8a
                 chunk = file.read(BUFFER_SIZE)
                 # TODO: section 2 step 8b
-                client_socket.send(chunk)
+                if not chunk:
+                    is_done = True
+                else:
+                    client_socket.send(chunk)
     except OSError as e:
         ret_value = 2
         print(f'An error occurred while sending the file:\n\t{e}')
@@ -74,7 +77,3 @@ if __name__ == "__main__":
 
     ret_value = send_file(file_name, (IP, PORT))
     sys.exit(ret_value)
-
-
-
-
